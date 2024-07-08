4921584e39d90d777da796e89aac8fc35cae91d0
manulatczuk <emanuelli.latczuk@gmail.com>
2024-07-08 03:43:07 -0300

    algumas atualizaçoes

--- backend/app/Http/Controllers/ValidateEmailcontroller.php
+++ backend/app/Http/Controllers/ValidateEmailcontroller.php
@@ -7,37 +7,33 @@ use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\api\AuthController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\JsonResponse;
 
 class ValidateEmailcontroller extends Controller
 {
-    public function verifyEmail(Request $request)
+    public function __construct()
     {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
-
-
-        
-        // Autentica o usuário
-        if (Auth::attempt($credentials)) {
-            $user = Auth::user();
-
-            // Verifica se o e-mail do usuário está verificado
-            if ($user->hasVerifiedEmail()) {
-                // E-mail verificado, redireciona para o dashboard ou outra rota
-                return redirect()->intended('/dashboard');
-            } else {
-                $user->sendEmailVerificationNotification();
-                // E-mail não verificado, desloga o usuário
-                Auth::logout();
-                return response()->json('Email Enviado');
-            }
-        }
-
-        // Credenciais inválidas
-        return back()->withErrors([
-            'email' => 'Credenciais inválidas.',
-        ]);
+        $this->middleware('auth');
     }
- }
+
+    public function sendVerificationLink(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $user = Auth::user();
+
+        if ($user->email === $request->email && !$user->hasVerifiedEmail()) {
+            $user->sendEmailVerificationNotification();
+            return response()->json(['message' => 'Link de verificação de email enviado com sucesso.'], 200);
+        } else {
+            return response()->json(['message' => 'Email já verificado ou não encontrado.'], 400);
+        }
+    }
+
+    public function verify(EmailVerificationRequest $request)
+    {
+        $request->fulfill();
+        return response()->json(['message' => 'Email verificado com sucesso.'], 200);
+    }
+}
