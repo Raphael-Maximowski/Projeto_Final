082ccfdff34bcb669180481b8328c50fd7b29119
Emanuelli Latczuk <emanuelli.latczuk@gmail.com>
2024-07-09 16:50:17 -0300

    atualizações no funil e coleções

--- backend/app/Http/Controllers/FunilController.php
+++ backend/app/Http/Controllers/FunilController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Funil;
+
 
 class FunilController extends Controller
 {
-    //
+    public function store(Request $request){
+
+        $request->validate([
+            'name'=> 'required|max:25',
+            'description' => 'required|max:255',
+        ]);
+    }
 }
