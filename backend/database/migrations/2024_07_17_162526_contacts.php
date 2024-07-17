1c1b638ebef8da7c49cdf3af0fb481559b7eb739
Emanuelli Latczuk <emanuelli.latczuk@gmail.com>
2024-07-17 16:42:02 -0300

    iniciei os contatos

---
+++ backend/database/migrations/2024_07_17_162526_contacts.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('posicao');
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('cpf');
+            $table->string('data de nascimento');
+            $table->string('endereco');
+            $table->decimal('value');
+            $table->timestamps();
+
+            $table->foreignId('step_id')->constrained()->onDelete('cascade');
+
+    });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
