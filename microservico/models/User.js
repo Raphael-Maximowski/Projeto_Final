592ff619d682575518e689a93e4afd13b1ff5e06
Emanuelli Latczuk <emanuelli.latczuk@gmail.com>
2024-07-25 16:17:47 -0300

    troquei um sinal

---
+++ microservico/models/User.js
@@ -0,0 +1,16 @@
+export default (sequelize, DataTypes) => {
+  return sequelize.define('users', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+    }
+  }, {timestamps: false});
+};
