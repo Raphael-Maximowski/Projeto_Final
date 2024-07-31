e763f162c1ff7ce59f502744500ab7f804e130f5
Emanuelli Latczuk <emanuelli.latczuk@gmail.com>
2024-07-31 16:31:03 -0300

    ainda to nos logs

--- backend/database/migrations/2024_07_31_142620_users_logs.php
+++ backend/database/migrations/2024_07_31_142620_users_logs.php
@@ -14,11 +14,13 @@ return new class extends Migration
             $table->string('actions');
             $table->string('details');
             $table->timestamp('created_at')->useCurrent();
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
         });
     }
 
     public function down(): void
     {
-        //
+        Schema::dropIfExists('users_logs');
     }
 };
